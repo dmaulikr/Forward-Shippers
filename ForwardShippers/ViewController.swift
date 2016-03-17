85deb58b7e7ee8c94b093b15489bb889c7cf9677
Kathleen Skinner <networking@katskinner.com>
2016-03-17 12:02:41 +0300

    Added very functional table to initial view controller.

--- ForwardShippers/ViewController.swift
+++ ForwardShippers/ViewController.swift
@@ -8,55 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    //Outlets
     @IBOutlet weak var WeightInput: UITextField!
     @IBOutlet weak var SubmitBtn: UIButton!
-    @IBOutlet weak var QuoteLbl: UILabel!
-    
-    //Properties
-   // var weight: Double = 0.0
- //   var aramexQuote: Double = 0.0
-    
-    //functions
+    //@IBOutlet weak var QuoteLbl: UILabel!
+
     @IBAction func SubmitBtnAction(sender: AnyObject) {
         //check for weight value
         if WeightInput.text != nil && WeightInput.text != "" {
             let weight = Double(WeightInput.text!)!
-            
-          let aramexQuote = aramexCalculate(weight)
-            
-            printQuote(weight,aramexQuote:aramexQuote)
+            let aramexQuote = aramexCalculate(weight)
+            let comgatewayQuote = comgatewayCalculate(weight)
+            printQuote(weight,aramexQuote:aramexQuote, comgatewayQuote:comgatewayQuote)
         }
        //No weight value
         else {
-            QuoteLbl.hidden = false
-            QuoteLbl.text = "Please insert a weight."
+           // QuoteLbl.hidden = false
+           // QuoteLbl.text = "Please insert a weight."
         }
     }
     
-    //calculate aramex
+    //calculate aramex shop and ship
     func aramexCalculate(weight: Double)-> Double {
-        let aramexQuote = weight + 1.0
-        return aramexQuote
+        let quote = weight + 1.0
+        return quote
+    }
+    
+    //calculate comgateway
+    func comgatewayCalculate(weight: Double) -> Double {
+        let quote = weight + 2.5
+        return quote
     }
     
     //print the quotes
-    func printQuote (weight: Double, aramexQuote: Double) {
- 
-        QuoteLbl.hidden = false
-        QuoteLbl.text = "To ship an item weighing \(weight) it will cost: \n something $dollars \(aramexQuote)"
+    func printQuote (weight: Double, aramexQuote: Double, comgatewayQuote: Double) {
+        //QuoteLbl.hidden = false
+        //QuoteLbl.text = "To ship an item weighing \(weight) it will cost: \n Aramex's Shop and Ship: $\(aramexQuote) \n Comgateway: $\(comgatewayQuote)"
     }
+    
+    
+    
+    var shippersArray = ["Shop and Ship", "Fish Is Fast", "Aramex", "Comagateway", "Post A Plus MyBox", "Borderlinx"]
+    let textCellIdentifier = "TextCell"
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shippersArray.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier( "textCellIdentifier", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = shippersArray[indexPath.row]
+       
+        //cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+  
+
 }
 
